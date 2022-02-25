0ec137d9bea5a5ffb9b4377139460d6613b58752
dined-io <38249237+dined-io@users.noreply.github.com>
2022-02-25 23:18:12 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dined',
+      version='0.1',
+      description='Dined - Anthropometry in Design',
+      url='http://github.com/dined-io/dyned',
+      author='Delft University of Technology',
+      author_email='dined-io@tudelft.nl',
+      license='BSD',
+      packages=['dined'],
+      zip_safe=False)
