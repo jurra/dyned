3dad4a5c005d3f4a482693953bc8198bf08c5bd7
jurra <jose.urra86@gmail.com>
2022-04-13 13:01:38 +0200

    updated packages and specified versions

--- setup.py
+++ setup.py
@@ -9,25 +9,25 @@ setup(name='dined',
       license='BSD',
       packages=find_packages(),
       install_requires=[
-            'numpy',
-            'pandas',
-            'scipy',
-            'bokeh',
-            'seaborn',
-            'openpyxl',
-            'requests'
+            'numpy==1.22.3',
+            'pandas==1.4.2',
+            'scipy==1.8.0',
+            'bokeh==2.4.2',
+            'seaborn==0.11.2',
+            'openpyxl==3.0.9',
+            'requests==2.27.1'
       ],
       extras_require={
         'dev': [
-            'pytest',
-            'pytest-pep8',
-            'pytest-cov',
-            'jupyter',
-            'notebook',
-            'pylint',
-            'python-dotenv',
-            'mysql-connector-python' #TODO: Remove later this is necessesary for exporting the current database to csvs
-
+            'pytest==7.1.1',
+            'pytest-pep8==1.0.6',
+            'pytest-cov==3.0.0',
+            'jupyter==1.0.0',
+            'notebook==6.4.10',
+            'pylint==2.13.5',
+            'python-dotenv==0.20.0',
+            'mysql-connector-python==8.0.28', #TODO: Remove later this is necessesary for exporting the current database to csvs
+            'SQLAlchemy==1.4.35'
         ]
       },
       zip_safe=False
