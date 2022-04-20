dc1542f5daab2ce56e4c722a99ed5f0e047f41fa
jurra <jose.urra86@gmail.com>
2022-04-20 15:54:56 +0200

    added experiments to the repository to share things we have been trying out.

--- dined/sql_to_csv.py
+++ dined/sql_to_csv.py
@@ -17,13 +17,14 @@ from os import environ as env
 import mysql.connector
 from mysql.connector import Error
 
-import pandas as pd
-
 from dotenv import load_dotenv
 load_dotenv()
 
 import json
 from sqlalchemy import create_engine
+
+import numpy as np
+import pandas as pd
 
 
 uri = f'mysql+mysqlconnector://{env["DB_USER"]}:{env["DB_PWD"]}@{env["DB_HOST"]}/{env["DB_NAME"]}'
@@ -31,7 +32,9 @@ db = create_engine(uri, echo=True)
 
 def read_measures_spec(path):
     with open (path) as file:
+        # TODO: Add check or asert
         return json.loads(file.read())
+
 
 def get_data(query="""SELECT * FROM studies;"""):
     '''Do sql query'''
@@ -61,6 +64,19 @@ def sql_to_df(query):
         result = pd.read_sql_query(query, connection)
         return pd.DataFrame(result)
 
+def mk_template(path):
+    ''' Makes a template for an individual based on the measures.json data'''
+    measures = read_measures_spec(path)
+
+    df = pd.DataFrame()
+    for measures_group in measures:
+        for measure in measures_group['labels']:
+                measure = pd.DataFrame([], columns={ measure['id']})
+                df = pd.concat([df, measure],ignore_index=True, axis=0)
+    
+    df.insert(0, 'individual_id', pd.Series)
+    return df    
+
 def write(query, path, columns=None):
     '''Write query results to csv '''
     records = get_data(query)
@@ -78,27 +94,46 @@ def write(query, path, columns=None):
 
 
 if __name__ == "__main__":
-    measures = read_measures_spec('../experiments/measures.json')
-    
-    # Here we create an empty dataframe where each individual has a row with all measurements
-    all_individuals = pd.DataFrame()
-    for measures_group in measures:
-        for measure in measures_group['labels']:
-                measure = pd.DataFrame([], columns={ measure['id']})
-                all_individuals = pd.concat([all_individuals, measure],ignore_index=True, axis=0)
-        # all_individuals.insert(0, 'Individual id', pd.Series)
-    
-    m = sql_to_df("SELECT * FROM measurements")
+    # Turn sql query result into pandas dataframe measurements
+    m = sql_to_df("SELECT * FROM measurements LIMIT 100;")
+    # Chop dataframe for testing
 
-    individual_id = 0
+    # Create the new dataframe with all the data
+    individuals = pd.DataFrame()
+
+    # Set individual id start
+    i_id = 0
+
+    # Makes individual empty dataframe to be populated
+    new_i = mk_template('../experiments/measures.json') # new individual
+    new_i.loc[len(new_i)] = np.nan
+    # Go through all the measurements to create dataframe
     for i, row in m.iterrows():
-        if row['individual_id'] == 1:
-            print(row)
-        else:
-            break
+        if int(row['individual_id']) != i_id:
+            # print(new_i)
+            if i_id > 0:
+                individuals = pd.concat([individuals, new_i], 
+                                        ignore_index=True, axis=0)
+                # clean the new_i dataframe to append to the aggregate indivduals
+                new_i.loc[len(new_i)] = np.nan
+                print(f'New Individual has id: {row["individual_id"]}')
+            i_id += 1
+
+        elif int(row['individual_id']) == i_id:
+            for col in new_i.columns:
+                if str(int(row['measure_id'])) == col: 
+                    new_i[col] = row['value']
+        
+
+
+    # df = new_i.dropna(axis=1, how='all')
+    # print(df)
+    df = individuals.dropna(axis=1, how='all')
+    print(df)
+
 
     # This function is deprecated as it is
     # write(sys.argv[1], sys.argv[2])
-
+    
 
 # Test conversion creation of dataframe for each individual
