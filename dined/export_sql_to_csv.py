a83482c4e831b8865cc112e34ea2a8a1ed4e5416
jurra <jose.urra86@gmail.com>
2022-08-15 12:02:12 +0200

    Cleaner code and added id to the files generated in the process, as there are duplicate names in the studies names.

--- dined/export_sql_to_csv.py
+++ dined/export_sql_to_csv.py
@@ -16,6 +16,8 @@ import pandas as pd
 import json
 import sqlalchemy
 
+from dined import load_measures_metadata
+
 
 if 'DB_PWD' in os.environ:
     del os.environ['DB_PWD']
@@ -25,59 +27,7 @@ load_dotenv()
 
 from sqlalchemy import create_engine
 
-uri = f'mysql+mysqlconnector://{env["DB_USER"]}:{env["DB_PWD"]}@{env["DB_HOST"]}/{env["DB_NAME"]}'
-
-def query_to_df(db, query: str) -> pd.DataFrame:
-    ''' Generic function to query the database and return a dataframe
-    '''
-    with db.connect() as connection:
-        result = pd.read_sql_query(query, connection)
-        return pd.DataFrame(result)
-
-def load_measures_spec(representation : json):
-    """Load the measures.json specification file into a flat list of dictionaries 
-    containing the measure id, name and unit"""
-    with open(representation) as json_file:
-        data = json.load(json_file)
-        # flatten dictionary
-        flat = []
-        # Flatten the measures
-        for measure in data:
-            for measure_group in measure['labels']:
-                flat.append(measure_group)
-
-        # sort id in ascending order by id
-        flat = sorted(flat, key=lambda x: int(x['id']))
-        return flat
-
-def print_measures_code(data):
-    """Quick check to traverse the measures.json specification and getting. 
-    The names associated with the ids
-    """
-    for measures_group in data:
-        for measure in measures_group['labels']:
-                print("This is the id:", measure['id'], "This is the measure name:" ,measure['name_en'])
-
-def write_measures_to_txt(data):
-    with open('measures,txt', 'w') as f:
-        for measures_group in data:
-            for measure in measures_group['labels']:
-                f.write("This is the id: " + measure['id'] + " This is the measure name: " + measure['name_en'] + "\n")
-
-# QUERIES 
-def get_tables(db):
-    query = 'SHOW TABLES'
-    return query_to_df(db, query)
-
-def get_measurements(db: sqlalchemy.engine.base.Engine, study_id=None) -> pd.DataFrame:
-    '''Gets measurement from the database, it can take a study id as an argument'''
-    query = 'SELECT * FROM measurements'
-    if study_id != None:
-        query += ' WHERE study_id = ' + study_id
-    return query_to_df(db, query)
-
-def write_to_csv(data, filename):
-    data.to_csv(filename, index=False)
+DB_URI = f'mysql+mysqlconnector://{env["DB_USER"]}:{env["DB_PWD"]}@{env["DB_HOST"]}/{env["DB_NAME"]}'
 
 def get_studies_names(studies_metadata: str) -> list:
     '''Returns a listh with the study id as key and the study name as value
@@ -88,20 +38,50 @@ def get_studies_names(studies_metadata: str) -> list:
     
     with open(studies_metadata) as json_file:
         data = json.load(json_file)
-
         # Return a list of dictionaries with the study id and its name  
         simplified_metadata = [{'study_id': study['id'], 'study_name': study['name']} for study in data]
-        
         return simplified_metadata
 
-def count_studies(db):
+def print_measures_code(measures: list) -> None:
+    """Quick check to traverse the measures.json specification and getting. 
+    The names associated with the ids
+    """
+    print_measure = lambda measure: print("This is the id:", measure['id'], "This is the measure name:" ,measure['name_en'])
+    list(map(print_measure, measures))
+
+def write_measures_to_txt(measures: list) -> None:
+    with open('measures.txt', 'w') as f:
+        # Here we use the map_measures_spec function to write the measures to a file
+        write_measure = lambda measure: f.write("This is the id: " + measure['id'] + " This is the measure name: " + measure['name_en'] + "\n")
+        list(map(write_measure, measures))
+
+# QUERIES 
+def query_to_df(db, query: str) -> pd.DataFrame:
+    ''' Generic function to query the database and return a dataframe
+    '''
+    with db.connect() as connection:
+        result = pd.read_sql_query(query, connection)
+        return pd.DataFrame(result)            
+
+def get_tables(db):
+    query = 'SHOW TABLES'
+    return query_to_df(db, query)
+
+def get_study_measurements(db: sqlalchemy.engine.base.Engine, study_id=None) -> pd.DataFrame:
+    '''Gets measurement from the database, it can take a study id as an argument'''
+    query = 'SELECT * FROM measurements'
+    if study_id != None:
+        query += ' WHERE study_id = ' + study_id
+    return query_to_df(db, query)
+
+def count_studies(db: sqlalchemy.engine.base.Engine) -> pd.DataFrame:
     ''' Returns amount of studies in the database
     '''
     query = 'SELECT COUNT(DISTINCT study_id) FROM measurements'    
     return query_to_df(db, query)
-
+    
 def measurements_per_individual(data: pd.DataFrame, measures_spec: list) -> pd.DataFrame:
-    '''Returns a dataframe that groups measurement_id in columns and individuales in rows
+    '''Returns a dataframe that groups measurement_id in columns and individuals in rows
     Example:
     individual_id | measure_id_1 | measure_id_2 | measure_id_3     | ...
     1             | value        | value        | value            | ...
@@ -109,14 +89,14 @@ def measurements_per_individual(data: pd.DataFrame, measures_spec: list) -> pd.D
     '''
 
     # Load specs
-    measures_spec = load_measures_spec(measures_spec)
+    measures_spec = load_measures_metadata(measures_spec)
 
     # Create columns for each measure_id
     columns_names = []
     columns_ids = []
+
     for measure_id in np.unique(data['measure_id']):
         # Get the measure spec that matches the measure_id
-        
         measure = [measure for measure in measures_spec if int(measure['id']) == measure_id]
         columns_ids.append(measure_id)
         columns_names.append(str(measure[0]['name_en']))
@@ -134,6 +114,10 @@ def measurements_per_individual(data: pd.DataFrame, measures_spec: list) -> pd.D
     df.columns = columns_names
     return df
 
+# WRITE TO CSV FUNCTIONS
+def write_to_csv(data, filename):
+    data.to_csv(filename, index=False)
+
 def export_raw_csv(db, studies_metadata: str, target_dir: str):
     '''For each study writes a csv file
     target_dir: directory where the csv files will be written
@@ -141,7 +125,6 @@ def export_raw_csv(db, studies_metadata: str, target_dir: str):
     # Count distinct studies
     query = 'SELECT DISTINCT study_id FROM measurements'
     studies = query_to_df(db, query)
-
     metadata = get_studies_names(studies_metadata)
 
     # For each study write a csv file
@@ -151,7 +134,7 @@ def export_raw_csv(db, studies_metadata: str, target_dir: str):
         study = [study for study in metadata if str(study['study_id']) == str(study_id)]
         study_name = study[0]['study_name']
         
-        data = get_measurements(db, study_id)
+        data = get_study_measurements(db, study_id)
         
         # Create studies directory if it doesnt exist
         if not os.path.exists(target_dir):
@@ -160,45 +143,42 @@ def export_raw_csv(db, studies_metadata: str, target_dir: str):
         write_to_csv(data, f'{target_dir}/{study_name}.csv')
 
 # Write formated data where each individual is a row
-def export_formatted_csv(db, studies_metadata, measures_spec, target_dir:str) -> None:
+# FIXME: Fix this entire messy function
+def export_formatted_csv(db: sqlalchemy.engine.base.Engine , 
+                        studies_metadata: str, 
+                        measures_spec: str,  
+                        target_dir: str) -> None:
     '''For each study writes a csv file'''
     # Count distinct studies
     query = 'SELECT DISTINCT study_id FROM measurements'
-    studies = query_to_df(db, query)
+    study_df = query_to_df(db, query)
 
-    metadata = get_studies_names(studies_metadata)
+    studies_names = get_studies_names(studies_metadata)
 
     # For each study write a csv file
-    for study_id in studies.study_id:
+    for study_id in study_df.study_id:
         study_id = str(study_id) # Make sure we compare the same types
         # get the study name
-        study = [study for study in metadata if str(study['study_id']) == str(study_id)]
-        study_name = study[0]['study_name'
-        ]
+        study = [study for study in studies_names if str(study['study_id']) == str(study_id)]
+        study_name = study[0]['study_name']
         # Avoid file name collisions
         study_name = study_name.replace(' ', '_')
         study_name = study_name.lower()
         
-        data = get_measurements(db, study_id)
+        data = get_study_measurements(db, study_id)
         df = measurements_per_individual(data, measures_spec)
     
         # Create studies directory if it doesnt exist
         if not os.path.exists(target_dir):
             os.makedirs(target_dir)
         # Create dataframe with measurements per individual
-        write_to_csv(df, f'{target_dir}/{study_name}.csv')
-
-# function to read csv study file
-def read_csv_study(filename:str) -> pd.DataFrame:
-    '''Reads a csv file and returns a dataframe'''
-    return pd.read_csv(filename)    
-    
+        write_to_csv(df, f'{target_dir}/{study_id}_{study_name}.csv')
     
 def main():
-    db = create_engine(uri, echo=True)
+    db = create_engine(DB_URI, echo=True)
     # For each study write a csv file
-    export_formatted_csv(db, './metadata/studies.json', './metadata/measures.json', './data')
-        
+    export_formatted_csv(db, './metadata/studies.json', './metadata/measures.json', './check-data')
+    return db
 
 if __name__ == '__main__':
     main()
